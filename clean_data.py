040f56ca576df27457fe9dbc95c66ce7e169ea86
Alexis Valencia Galeano <39319649+Alexisvag@users.noreply.github.com>
2024-03-08 18:28:46 -0500

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
-
+    df = pd.read_csv(input_file)
+    return df
 
 def create_fingerprint(df):
     """Cree una nueva columna en el DataFrame que contenga el fingerprint de la columna 'text'"""
@@ -20,6 +21,20 @@ def create_fingerprint(df):
     # 7. Transforme cada palabra con un stemmer de Porter
     # 8. Ordene la lista de tokens y remueve duplicados
     # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
+    df = df.copy()
+    df["key"] = df["text"]
+    df["key"] = df["key"].str.strip()
+    df["key"] = df["key"].str.lower()
+    df["key"] = df["key"].str.replace("-", "")
+    df["key"] = df["key"].str.translate(
+        str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    )
+    df["key"] = df["key"].str.split()
+    stemmer = nltk.PorterStemmer()
+    df["key"] = df["key"].apply(lambda x: [stemmer.stem(word) for word in x]) 
+    df["key"] = df["key"].apply(lambda x: sorted(set(x))) 
+    df["key"] = df["key"].str.join(" ")
+    return df
 
 
 def generate_cleaned_column(df):
@@ -31,7 +46,16 @@ def generate_cleaned_column(df):
     # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
     # 3.  Cree un diccionario con 'fingerprint' como clave y 'text' como valor
     # 4. Cree la columna 'cleaned' usando el diccionario
+    df = df.sort_values(by=["key", "text"], ascending=[True, True])
+    keys = df.drop_duplicates(subset="key", keep="first")
 
+    return keys
+
+
+df = load_data("input.txt")
+df = create_fingerprint(df)
+df = generate_cleaned_column(df)
+print(df)
 
 def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
@@ -49,8 +73,8 @@ def main(input_file, output_file):
     save_data(df, output_file)
 
 
-if __name__ == "__main__":
-    main(
-        input_file="input.txt",
-        output_file="output.txt",
-    )
+# if __name__ == "__main__":
+#     main(
+#         input_file="input.txt",
+#         output_file="output.txt",
+#     )
